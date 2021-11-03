d4c70f791fbe0e7da412fdd6de7e6b0d2bebfb84
colinkiama <colinkiama@gmail.com>
2021-11-03 01:24:24 +0000

    Parses error codes

--- linter.py
+++ linter.py
@@ -1,9 +1,9 @@
-from SublimeLinter.lint import Linter  # or NodeLinter, PythonLinter, ComposerLinter, RubyLinter
+from SublimeLinter.lint import Linter
 
 
-class __class__(Linter):
-    cmd = 'io.elementary.vala-lint'
-    regex = r'^\s*(?P<line>\d+)\.(?P<col>\d+)\s*(?:(?P<error>error)|(?P<warning>warn))\s*(?P<message>.+)'
+class ValaLint(Linter):
+    cmd = 'io.elementary.vala-lint ${file}'
+    regex = r'^\s*(?P<line>\d+)\.(?P<col>\d+)\s*(?:(?P<error>error)|(?P<warning>warn))\s*(?P<message>(.\S{1,})+)\s*(?P<code>(.\S{1,})+)'
     multiline = False
     defaults = {
         'selector': 'source.vala'
